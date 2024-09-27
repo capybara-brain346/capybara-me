9885ad8aba8267e34ed2ce8a57a166fe70512fd4
Piyush Choudhari <choudhari.piyush@gmail.com>
2024-09-27 06:47:56 +0530

    Added personal details

--- components/sections/projects/featured.jsx
+++ components/sections/projects/featured.jsx
@@ -17,9 +17,9 @@ export default function FeaturedProjects() {
 		<Section classProp={css.hasBg}>	
 			<Container spacing={'verticalXXXXLrg'}>
 				<SectionTitle
-					title="Featured Projects"
-					preTitle="UX and Full Stack"
-					subTitle="Focused on the experience, driven by the engineering."
+					title="Featured Projects 🌟"
+					preTitle=""
+					subTitle=""
 				/> 				{
 				content.map( (data, index) => {
 					return (
